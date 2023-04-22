ee94e5ed3273a823c6a8bb506fddefb797a33232
Svetlana Dmitrieva <53534598+Silenzion@users.noreply.github.com>
2023-04-22 13:24:26 +0300

    Remove tailwind,updated _variables.scss

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,7 @@ import BaseButton from "@/assets/components/BaseButton.vue";
   <div class="flex min-h-screen items-center justify-center">
     <div class="base-card max-w-[415px]">
       <div class="mb-24 flex w-full flex-row items-center justify-center">
-        <!--        <button class="base-btn_circle"><i class="fas fa-arrow-left"></i></button>-->
         <div class="uppercase text-gray-350">Playing now</div>
-        <!--        <button class="base-btn_circle"><i class="fas fa-bars"></i></button>-->
       </div>
       <img class="base-album mx-auto mb-24 rounded-full" src="https://placeimg.com/150/150/any" alt=" " />
       <div class="font-medium">Malice Mizer</div>
--- src/assets/store/index.ts
+++
@@ -1,8 +0,0 @@
-import { defineStore } from "pinia";
-
-// export type RootState = {
-// };
-
-export const useMainStore = defineStore({
-  id: "mainStore",
-});
