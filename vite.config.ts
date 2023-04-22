888bca3dad5a648a3b589f8146752b72d18680e9
Svetlana Dmitrieva <53534598+Silenzion@users.noreply.github.com>
2023-04-22 14:20:46 +0300

    Build is updated, removed hash from files' names

--- vite.config.ts
+++ vite.config.ts
@@ -23,5 +23,12 @@ export default defineConfig({
   build: {
     outDir: path.resolve(__dirname, "docs"),
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
   },
 });
